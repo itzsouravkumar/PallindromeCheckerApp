77a62b24003cca11a8d711aa5b4899782d1224b4
itzsouravkumar <oyesourav05012006@gmail.com>
2026-02-17 14:42:33 +0530

    Sourav Kumar Added UC1-WelcomePage

---
+++ App/src/PalindromeChecker.java
@@ -0,0 +1,21 @@
+/**
+MAIN CLASS - UseCase1PalindromeApp
+Use Case 1: Application Entry & Welcome MessageDescription:
+ This class represents the entry point of the
+ Palindrome Checker Management System.
+At this stage, the application:
+ Starts execution from the main() method
+ Displays a welcome message
+ Shows application version
+ No palindrome logic is implemented yet.
+
+ The goal is to establish a clear startup flow.
+ @author Sourav Kumar
+ @version 1.0
+ **/
+
+public class PalindromeChecker {
+    static void main(String [] args) {
+        System.out.print("Welcome to Palindrome Checker Management System \nVersion : 1.0 \nSystem initialised successfully.");
+    }
+}
